1a662ea4b4c560a83ad1db4b72a97a91c5912710
sbsalvi <sbsalvi500@gmail.com>
2025-02-05 22:29:25 +0600

    django app and theory update

--- day4/TestProject2/TestProject2/asgi.py
+++ day4/TestProject2/TestProject2/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Test2Project project.
+ASGI config for TestProject2 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Test2Project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TestProject2.settings')
 
 application = get_asgi_application()
--- day4/TestProject2/TestProject2/urls.py
+++ day4/TestProject2/TestProject2/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('fastapp/', include('fastapp.urls'))
+    path('firstapp/', include('FirstAPP.urls'))
 
 ]
